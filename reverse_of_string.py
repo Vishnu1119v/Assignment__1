a0eb621ab85a08a3d3c9eb717909a503f66b80d9
Vishnu1119v <105714872+Vishnu1119v@users.noreply.github.com>
2022-11-17 22:14:27 +0530

    question 2

--- reverse_of_string.py
+++ reverse_of_string.py
@@ -1,12 +1,5 @@
-
-#sentence = input("Enter sentence: ")
-#longest = sorted(sentence.split(),key=len)
-#print("Longest word is: ", longest[-1])
-str1=input("Enter sentence: ")
-str2=str1.split()
-max_len=-1
-for i in str2:
-    if len(i)>max_len:
-      max_len=len(i)
-    result=i
-print(result)
+string=input("enter the string :")
+rev_string=''
+for i in string:
+    rev_string=i+rev_string
+print(rev_string)
